a4ec5438538664b02a6e8149964330dbbf317eec
ben <xyers1@gmail.comn>
2022-03-09 00:38:15 +0800

    Added Date Parser for due date commment, checker for card task within org space and due complete

--- functions/availability.js
+++ functions/availability.js
@@ -1,19 +1,20 @@
 const _ = require("lodash");
 
+// IDS of organization and board
+// var GMWW_org_id = "61ed74813f38718d7cf12236"
+var org_boards_dict = {
+  GMWW_masterboard_id: "62026ee9c1ccb205ac50626e",
+  GMWW_ideasboard_id: "61f2c1c19be3de171182c243",
+  GMWW_sermonboard_id: "61f6563244c03a134fe8ccc6",
+  GMWW_singsboard_id: "61f6563ed7993620ea668677",
+  GMWW_technicalboard_id: "61f2c9505d6e7a72cc0b7ca0"
+}
+
 module.exports = {
   Update: async function (key, token, org_members) {
     var Trello = require("trello");
     var trello = new Trello(key, token);
 
-    // IDs
-    var GMWW_org_id = "61ed74813f38718d7cf12236"
-    var org_boards_dict = {
-      GMWW_masterboard_id: "62026ee9c1ccb205ac50626e",
-      GMWW_ideasboard_id: "61f2c1c19be3de171182c243",
-      GMWW_sermonboard_id: "61f6563244c03a134fe8ccc6",
-      GMWW_singsboard_id: "61f6563ed7993620ea668677",
-      GMWW_technicalboard_id: "61f2c9505d6e7a72cc0b7ca0"
-    }
     // Members availability lists that contains status cards
     var members_available_list = "6204e84ae945aa1d2201cfbf"
     var members_occupied_list = "6204e84e13783e4a366412b7"
@@ -34,8 +35,7 @@ module.exports = {
           console.log(`[Availability]: Checking on ${member.fullName} card now`)
           comment_to_make = get_comment_to_add(card);
           // If its still due, add assigned card link to member's status card and move to occupied
-          // TODO : Check card is really a tasks
-          if (is_card_outstanding(card)) {
+          if (is_card_inside_GBWMM_org(card) && !card.dueComplete) {
             is_assigned = true
             // Check if its already on members' status card, if not add comment
             const to_comment = !await is_assigned_comment_already_in_status_card(comment_to_make, member.statusCardId, trello)
@@ -67,8 +67,9 @@ module.exports = {
 
 // Private functions
 
-function is_card_inside_org_board(card_id, org_boards_dict) {
-  return Object.values(org_boards_dict).includes(card_id);
+function is_card_inside_GBWMM_org(card_obj) {
+  var board_id_of_card = card_obj.idBoard;
+  return Object.values(org_boards_dict).includes(board_id_of_card);
 }
 
 function is_card_outstanding(card_obj) {
@@ -81,9 +82,11 @@ function is_card_outstanding(card_obj) {
 function get_comment_to_add(members_assigned_card) {
   var newline = "\n"
   var comment = "Task: "
-  comment += members_assigned_card.url
+  comment += members_assigned_card.url + newline
   if (members_assigned_card.due) {
-    comment += "Due Date: " + members_assigned_card.due
+    var due_date = new Date(members_assigned_card.due)
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    comment += "Due Date: " + due_date.toLocaleDateString("en-US", options)
   }
   return comment
 }
